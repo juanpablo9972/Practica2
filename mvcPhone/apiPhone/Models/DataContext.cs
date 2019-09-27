7840a8311dc833a5a05f2083903b72ec05b77454
visual_community@outlook.com <visual_community@outlook.com>
2019-09-27 13:17:54 -0400

    Web App y WebApi finalizado

---
+++ mvcPhone/apiPhone/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+namespace apiPhone.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<apiPhone.Models.Phone> Phones { get; set; }
+    }
+}
---
+++ mvcPhone/apiPhone/Models/Phone.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apiPhone.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum TypeContact
+    {
+        Facebook,
+        Twitter,
+        LinkedIN,
+        Instagram
+
+    }
+
+    public class Phone
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public TypeContact Type { get; set; }
+
+        [Required]
+        public string Contact { get; set; }
+
+    }
+}
